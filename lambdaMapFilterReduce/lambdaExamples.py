6359294356afbc4ddacb26aa23274ae1145146e2
setty leela prasanna venkateswara <140870023+LeelaPrasannaDev@users.noreply.github.com>
2024-06-22 12:32:30 +0530

    lambda problems

---
+++ lambdaMapFilterReduce/lambdaExamples.py
@@ -0,0 +1,14 @@
+# lambda with one argument 
+
+add = lambda x: x+10
+print(add(20)) # 20+10 = 30
+
+# lambda with two arguments
+
+addTwo = lambda a,b : a+b
+print(addTwo(10,20)) #30
+
+#lambda with three arguments
+
+multiplyThree = lambda c,d,e : c*d*e
+print(multiplyThree(10,20,30)) # 6000
